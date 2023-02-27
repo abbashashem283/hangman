8c85592ee8ae0af3f8abc09091d800a345d30cc0
Abbas <abbashashem283@gmail.com>
2023-02-27 21:49:12 +0200

    created function to determine what portion of string to show based on length

---
+++ game.js
@@ -0,0 +1,13 @@
+const data = {
+    "Magician":"A Person who performs magic",
+    "Carpenter": "A Person who works with wood",
+    "Black Smit": "A Person who shapes iron"
+}
+
+
+
+
+function lettersToShow(word_length){
+    return Math.floor(word_length * 0.3) ;
+}
+
