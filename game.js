ede0b44a241e58d6e0b6eef1c1cbc48cbd61f6bc
Abbas <abbashashem283@gmail.com>
2023-02-27 22:03:47 +0200

    added function to choose random word

--- game.js
+++ game.js
@@ -1,13 +1,28 @@
 const data = {
     "Magician":"A Person who performs magic",
     "Carpenter": "A Person who works with wood",
-    "Black Smit": "A Person who shapes iron"
+    "Black Smith": "A Person who shapes iron"
+}
+
+let chosen_word
+
+
+function getChosenWord() {
+    let keys = Object.keys(data);
+    let index = getRandomNumber(keys.length);
+    return keys[index]
 }
 
 
-
+function getRandomNumber(range) {
+    return Math.floor(Math.random() * range)
+}
 
 function lettersToShow(word_length){
-    return Math.floor(word_length * 0.3) ;
+    return Math.floor(word_length * 0.3)
+}
+
+function positionsOfDisplay() {
+
 }
 
