83b932e1230fc13d6d0cc65a929bd9df048685af
Abbas <abbashashem283@gmail.com>
2023-03-10 15:28:07 +0200

    added python git controller

---
+++ git-controller.py
@@ -0,0 +1,40 @@
+import os
+
+def add():
+    os.system(f"git add .")
+
+def commit():
+    message = input("enter commit message:\n")
+    os.system(f'git commit -m "{message}"')
+
+def status():
+    os.system("git status")
+
+def pull():
+    os.system("git pull")
+
+def push():
+    os.system("git push")
+
+def record():
+    add()
+    commit()
+    pull()
+    push()
+
+
+while True:
+    print("===============================\n")
+    command = input("a: add | c: commit | s: status | push: push | pull: pull | r: record\n")
+    if command == "a":
+        add()
+    elif command == "c":
+        commit()
+    elif command == "s":
+        status()
+    elif command == "push":
+        push()
+    elif command == "pull":
+        pull()
+    elif command == "r":
+        record()
